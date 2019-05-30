6f26ff2d147863ad91385245faa68bb81517f3c7
Aaron B <abriscoe2008@gmail.com>
2019-05-30 23:11:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,7 @@ shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest[gun "powder"]
+shipping_manifest["gun "powder""]
 
   # return the shipping_manifest hash below
 
